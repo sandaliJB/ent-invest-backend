59352901e0fc8ff3c15ab791da4e12c150c498bf
sandaliJB <jayasinghesb2000@gmail.com>
2025-01-10 20:46:04 +0530

    initial commit

---
+++ src/main/java/com/enterprise/ent_invest_backend/EntInvestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.enterprise.ent_invest_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntInvestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntInvestBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enterprise/ent_invest_backend/EntInvestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enterprise.ent_invest_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntInvestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
