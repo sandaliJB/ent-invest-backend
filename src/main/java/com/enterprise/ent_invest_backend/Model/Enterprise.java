6eedce826fa8a0416894f498e8a7593a857dd96b
sandaliJB <jayasinghesb2000@gmail.com>
2025-01-10 22:23:39 +0530

    Implement enterprise management functionality with CRUD operations and MongoDB integration.

---
+++ src/main/java/com/enterprise/ent_invest_backend/Model/Enterprise.java
@@ -0,0 +1,135 @@
+package com.enterprise.ent_invest_backend.Model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "enterprise")
+public class Enterprise {
+    @Id
+    private String enterpriseId;
+    private String userId;
+    private String enterpriseName;
+    private String enterpriseEmail;
+    private String registerNumber;
+    private String enterpriseType;
+    private String startingDate;
+    private String address;
+    private String city;
+    private String telNumber;
+    private String webUrl;
+    private String imageName;
+    private byte[] imageData;
+    private String contentType;
+
+    public String getEnterpriseId() {
+        return enterpriseId;
+    }
+
+    public void setEnterpriseId(String enterpriseId) {
+        this.enterpriseId = enterpriseId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getEnterpriseName() {
+        return enterpriseName;
+    }
+
+    public void setEnterpriseName(String enterpriseName) {
+        this.enterpriseName = enterpriseName;
+    }
+
+    public String getEnterpriseEmail() {
+        return enterpriseEmail;
+    }
+
+    public void setEnterpriseEmail(String enterpriseEmail) {
+        this.enterpriseEmail = enterpriseEmail;
+    }
+
+    public String getRegisterNumber() {
+        return registerNumber;
+    }
+
+    public void setRegisterNumber(String registerNumber) {
+        this.registerNumber = registerNumber;
+    }
+
+    public String getEnterpriseType() {
+        return enterpriseType;
+    }
+
+    public void setEnterpriseType(String enterpriseType) {
+        this.enterpriseType = enterpriseType;
+    }
+
+    public String getStartingDate() {
+        return startingDate;
+    }
+
+    public void setStartingDate(String startingDate) {
+        this.startingDate = startingDate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelNumber() {
+        return telNumber;
+    }
+
+    public void setTelNumber(String telNumber) {
+        this.telNumber = telNumber;
+    }
+
+    public String getWebUrl() {
+        return webUrl;
+    }
+
+    public void setWebUrl(String webUrl) {
+        this.webUrl = webUrl;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+
+    public byte[] getImageData() {
+        return imageData;
+    }
+
+    public void setImageData(byte[] imageData) {
+        this.imageData = imageData;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+}
---
+++ src/main/java/com/enterprise/ent_invest_backend/Repository/EnterpriseRepository.java
@@ -0,0 +1,12 @@
+package com.enterprise.ent_invest_backend.Repository;
+
+import com.enterprise.ent_invest_backend.Model.Enterprise;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface EnterpriseRepository extends MongoRepository<Enterprise, String> {
+    List<Enterprise> findByUserId(String userId);
+    List<Enterprise> findByEnterpriseName(String enterpriseName);
+    List<Enterprise> findByCity(String city);
+}
