b4a1f5f3216ac4bf4495e85c4ca5999a64297523
sandaliJB <jayasinghesb2000@gmail.com>
2025-01-11 15:33:55 +0530

    Implement message management functionality with CRUD operations and MongoDB integration

---
+++ src/main/java/com/enterprise/ent_invest_backend/Service/MessageService.java
@@ -0,0 +1,16 @@
+package com.enterprise.ent_invest_backend.Service;
+
+import com.enterprise.ent_invest_backend.Model.Message;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MessageService {
+    public Message createMessage(Message message);
+    public List<Message> getAllMessages();
+    public List<Message> getMessageByUserId(String userId);
+    public Optional<Message> getMessageById(String messageId);
+    public Message updateMessage(String messageId, Message message);
+    public void deleteMessage(String messageId);
+    public Message updateStatus(String messageId);
+}
