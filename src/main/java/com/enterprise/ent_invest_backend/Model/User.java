f17e5f57e1530196f69ea1218f1bcae4fa4caae4
sandaliJB <jayasinghesb2000@gmail.com>
2025-01-19 12:45:43 +0530

    Add Investment, Meeting, Message, and User models with MongoDB integration and Lombok support

---
+++ src/main/java/com/enterprise/ent_invest_backend/Model/User.java
@@ -0,0 +1,68 @@
+package com.enterprise.ent_invest_backend.Model;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "user")
+public class User {
+    @Id
+    private String userId;
+    private String userEmail;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String userRole = "USER";
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+}
